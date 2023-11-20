ae2589d77a9c94ff0880d62055f86e5080e52f5f
Bảo Trúc <114350927+BaoTruc0605@users.noreply.github.com>
2023-11-21 04:02:11 +0700

    Lan5: upload App.js

--- BIDV/App.js
+++ BIDV/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
 import Home from './components/Home'
+import Login from './components/Login'
 import Screen03 from './components/Screen03'
 import Screen04 from './components/Screen04'
 import Screen05 from './components/Screen05'
+import Screen06 from './components/Screen06'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -11,11 +13,15 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
+        {/* <Stack.Screen name="MuaSam" component={MuaSam} />  */}
+
         <Stack.Screen name="Screen03" component={Screen03} />
         <Stack.Screen name="Screen04" component={Screen04} />
         <Stack.Screen name="Screen05" component={Screen05} />
+        <Stack.Screen name="Screen06" component={Screen06} />
       </Stack.Navigator>
     </NavigationContainer>
   );
