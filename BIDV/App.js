038b51e7ad818ccb8ba5d74f2953171f3b3a696f
Bảo Trúc <114350927+BaoTruc0605@users.noreply.github.com>
2023-11-24 22:53:02 +0700

    lan9: use Provider

--- BIDV/App.js
+++ BIDV/App.js
@@ -1,32 +1,41 @@
-import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
+import Home from './components/Home'
+import Login from './components/Login'
+import Screen03 from './components/Screen03'
+import Screen04 from './components/Screen04'
+import Screen05 from './components/Screen05'
+import Screen06 from './components/Screen06'
+import Screen07 from './components/Screen07'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import login from './components/Login'
-import home from './components/Home'
-import screen03 from './components/Screen03'
-import screen04 from './components/Screen04'
-import screen05 from './components/Screen05'
-import screen06 from './components/Screen06'
-import screen07 from './components/Screen07';
-import screen08 from './components/Screen08';
+import Footer from './components/Footer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { UserProvider } from './components/UserProvider';
 
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+
+
 export default function App() {
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen04">
-        <Stack.Screen name="Login" component={login} />
-        <Stack.Screen name="Home" component={home} />
-        <Stack.Screen name="Screen03" component={screen03} />
-        <Stack.Screen name="Screen04" component={screen04} />
-        <Stack.Screen name="Screen05" component={screen05} />
-        <Stack.Screen name="Screen06" component={screen06} />
-        <Stack.Screen name="Screen07" component={screen07} />
-        <Stack.Screen name="Screen08" component={screen08} />
+    <UserProvider>
+      <NavigationContainer >
 
-      </Stack.Navigator>
-    </NavigationContainer>
+        <Tab.Navigator tabBar={(props) => <Footer {...props} />}>
+          <Tab.Screen name="Login" component={Login} />
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Screen03" component={Screen03} />
+          <Tab.Screen name="Screen04" component={Screen04} />
+          <Tab.Screen name="Screen05" component={Screen05} />
+          <Tab.Screen name="Screen06" component={Screen06} />
+          <Tab.Screen name="Screen07" component={Screen07}
+
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 }
+
