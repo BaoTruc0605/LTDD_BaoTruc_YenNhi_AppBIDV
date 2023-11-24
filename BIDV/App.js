41fd31eb8d05d3eee4de50ad00288da2b71219cd
Bảo Trúc <114350927+BaoTruc0605@users.noreply.github.com>
2023-11-25 02:12:08 +0700

    Lan10: Biến động số dư, lưu người thụ hưởng

--- BIDV/App.js
+++ BIDV/App.js
@@ -7,6 +7,7 @@ import Screen04 from './components/Screen04'
 import Screen05 from './components/Screen05'
 import Screen06 from './components/Screen06'
 import Screen07 from './components/Screen07'
+import Screen08 from './components/Screen08'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Footer from './components/Footer';
@@ -30,9 +31,8 @@ export default function App() {
           <Tab.Screen name="Screen04" component={Screen04} />
           <Tab.Screen name="Screen05" component={Screen05} />
           <Tab.Screen name="Screen06" component={Screen06} />
-          <Tab.Screen name="Screen07" component={Screen07}
-
-          />
+          <Tab.Screen name="Screen07" component={Screen07}/>
+          <Tab.Screen name="Screen08" component={Screen08}/>
         </Tab.Navigator>
       </NavigationContainer>
     </UserProvider>
